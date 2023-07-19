37db7c973417e1a4fca3982a0f5956ad96db74e4
Yahya <hyahya918@gmail.com>
2023-07-19 18:14:42 +0300

    last section is to add shapes

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast () {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += ' dark-theme';
+    } else {
+        document.body.classList.remove('dark-theme');
+    }
+}
+
+
 function contact (event) {
     event.preventDefault();
-   /* emailjs
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += ' modal__overlay--visible';
+
+    emailjs
         .sendForm(
             'service_586o5bp',
             'template_99fu5i4',
             event.target,
             'UZTrOdcxQFPDPffCL'
         ).then (()=> {
-            console.log('it works');
-        }) */
-        const loading = document.querySelector('.modal__overlay-load');
-        const success = document.querySelector('.modal__overlay--success');
-        setTimeout(() => {
-            console.log('it worked');
-        })
+            loading.classList.remove('modal__overlay--visible');
+            success.classList += ' modal__overlay--visible';    
+        }).catch(() => {
+            loading.classList.remove('modal__overlay--visible');
+            alert('The email service is temporarily unavailable. Please contact me directly on hyahya918@gmail.com');
+        }); 
 };
+
+
+function toggleModal () {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove('modal--open');
+    }
+    isModalOpen = true;
+    document.body.classList += ' modal--open';
+}
