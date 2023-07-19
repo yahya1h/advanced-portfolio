507074e05bf9cefbd00cea629a2033feb996fbee
Yahya <hyahya918@gmail.com>
2023-07-20 01:16:16 +0300

    finished project, just need to fix form

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1/20;
+
+function moveBackground() {
+    const shapes = document.querySelectorAll('.shape');
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const booleanInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * booleanInt}px, ${y * booleanInt}px)`;
+    }
+}
 
 function toggleContrast () {
     contrastToggle = !contrastToggle;
